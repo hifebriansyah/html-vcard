4d9aa47f711368ad4aedbcf2d0c071611516e672
hifebriansyah <hifebriansyah@gmail.com>
2018-12-10 22:46:18 +0700

    PWA support

---
+++ sw.js
@@ -0,0 +1,26 @@
+var cacheName = 'v1.1';
+var filesToCache = [
+  '',
+  'index.html',
+  'static/styles/vcard.css',
+  'static/libs/font-awesome/font-awesome.min.css'
+];
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
